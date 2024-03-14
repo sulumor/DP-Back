e4a2d110630fa5d5b18d070df97a4e40df2d33f0
sulumor <romuald.patfoort@gmail.com>
2024-03-14 21:41:02 +0100

    feat: function get invoice details

--- app/controllers/invoice.controller.js
+++ app/controllers/invoice.controller.js
@@ -3,4 +3,10 @@ import InvoiceDatamapper from "../datamapper/invoice.datamapper.js";
 
 export default class InvoiceController extends CoreController {
   static datamapper = InvoiceDatamapper;
+
+  static async getInvoiceDetails({ user }, res, next) {
+    const invoices = await this.datamapper.invoiceWithDetails(user);
+    if (!invoices) return next();
+    return res.status(200).json(invoices);
+  }
 }
--- app/datamapper/invoice.datamapper.js
+++ app/datamapper/invoice.datamapper.js
@@ -1,9 +1,16 @@
+import client from "../helpers/pg.client.js";
 import CoreDatamapper from "./core.datamapper.js";
 
 export default class InvoiceDatamapper extends CoreDatamapper {
   static tableName = "invoice";
 
-  static insertTable = "insert_invoice";
+  static insertTable = "add_invoice";
 
   static updateTable = "update_invoice";
+
+  static async invoiceWithDetails({ name }) {
+    // eslint-disable-next-line quotes
+    const result = await client.query(`SELECT * FROM "invoice_details" WHERE name=$1`, [name]);
+    return result.rows;
+  }
 }
