9d90d0236cf69cb77e9938e63d7edfc15da97fc4
sulumor <romuald.patfoort@gmail.com>
2024-03-14 21:39:04 +0100

    feat: MEP authentification

--- app/routers/api/index.router.js
+++ app/routers/api/index.router.js
@@ -4,9 +4,11 @@ import visitorRouter from "./visitors/index.router.js";
 import productRouter from "./products/index.router.js";
 import invoiceRouter from "./invoices/index.router.js";
 import invoiceLineRouter from "./invoice_lines/index.router.js";
+import authenticateToken from "../../middlewares/authorization.middleware.js";
 
 const apiRouter = Router();
 
+apiRouter.use(authenticateToken);
 apiRouter.use("/visitors", visitorRouter);
 apiRouter.use("/products", productRouter);
 apiRouter.use("/invoices", invoiceRouter);
--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,8 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import apiRouter from "./api/index.router.js";
+import authRouter from "./auth/index.router.js";
 
-const router = express.Router();
+const router = Router();
 
 router.use("/api", apiRouter);
+router.use("/", authRouter);
 
 export default router;
