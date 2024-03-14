1d90fa1dc352052d2ce81d5451c5404fe10d9c1c
sulumor <romuald.patfoort@gmail.com>
2024-03-14 21:32:13 +0100

    feat: install cookieParser and cors

--- app/index.app.js
+++ app/index.app.js
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import router from "./routers/index.router.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import createDoc from "./helpers/swagger.doc.js";
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONT_URL || "*",
+  credentials: true,
+};
+
 createDoc(app);
 
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
